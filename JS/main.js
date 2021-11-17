c3d2ba2c16b4f14d5efc4c2801997afdab204d3b
Alex MArgaryan <margaryanax@gmail.com>
2021-11-17 15:36:42 +0400

    new

--- JS/main.js
+++ JS/main.js
@@ -7,18 +7,38 @@ let value;
 let plus = 0;
 let freeSpin = 5;
 let prize;
+const infoArr = [
+    'TRY AGAIN',
+    '400$',
+    'TRY AGAIN',
+    '200$',
+    'TRY AGAIN',
+    '300$',
+    'TRY AGAIN',
+    '100$'
+];
+let totalCaunt = infoArr.length;
+console.log(totalCaunt);
+let angle = 360 / totalCaunt;
+console.log(angle);
 
 function rotate() {
     let random = Math.floor(Math.random() * 360);
     console.log(random);
-    random >= 1 && random <= 45 ? info(0, 'TRY AGAIN') :
-        random >= 46 && random <= 90 ? info(45, '100$') :
-        random >= 91 && random <= 135 ? info(90, 'TRY AGAIN') :
-        random >= 136 && random <= 180 ? info(135, '300$') :
-        random >= 181 && random <= 225 ? info(180, 'TRY AGAIN') :
-        random >= 226 && random <= 270 ? info(225, '200$') :
-        random >= 271 && random <= 315 ? info(270, 'TRY AGAIN') :
-        random >= 316 && random <= 360 ? info(315, '400$') : '';
+    infoArr.map((i, index) => {
+
+        random > index * angle && random <= (index + 1) * angle ?
+            info(index * angle, i) : index + 1;
+        console.log(index);
+    })
+    // random >= 1 && random <= 45 ? info(0, 'TRY AGAIN') :
+    //     random >= 46 && random <= 90 ? info(45, '400$') :
+    //     random >= 91 && random <= 135 ? info(90, 'TRY AGAIN') :
+    //     random >= 136 && random <= 180 ? info(135, '200$') :
+    //     random >= 181 && random <= 225 ? info(180, 'TRY AGAIN') :
+    //     random >= 226 && random <= 270 ? info(225, '300$') :
+    //     random >= 271 && random <= 315 ? info(270, 'TRY AGAIN') :
+    //     random >= 316 && random <= 360 ? info(315, '100$') : '';
     console.log(value);
     spincount.innerHTML = freeSpin--;
     plus += 5 * 360;
@@ -32,9 +52,14 @@ function rotate() {
     prizeElement.classList.add('box')
     wheel.style.transform = `rotate(${value+plus}deg)`;
 }
+
+
+
 btn.addEventListener('click', () => {
     freeSpin >= 0 ? rotate() : "";
+    setTimeout(function () {
+        return prizeBox.append(prizeElement);
+    }, 2000);
 
-    prizeBox.append(prizeElement);
 });
 spincount.innerHTML = freeSpin--;
